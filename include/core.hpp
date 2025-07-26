296f3f78b09cf681a5b26e436001cb4a77e8b07f
yqcr233 <3060188944@qq.com>
2025-07-26 17:18:49 +0800

    Adjust the project structure

---
+++ include/core.hpp
@@ -0,0 +1,6 @@
+#ifndef CORE_HPP
+#define CORE_HPP
+#include<iostream>
+
+using namespace std;
+#endif
---
+++ include/simple_memory_pool.hpp
@@ -0,0 +1,9 @@
+#ifndef SIMPLE_MEMORY_POOL_HPP
+#define SIMPLE_MEMORY_POOL_HPP
+
+#include "core.hpp"
+
+class SimpleMemoryPool{
+
+};
+#endif
