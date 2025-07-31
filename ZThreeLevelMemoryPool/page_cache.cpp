28e03bdd1d0f5771825d5cab9b14e197155ea596
yqcr233 <3060188944@qq.com>
2025-07-31 16:18:22 +0800

    init PageCache

--- ZThreeLevelMemoryPool/page_cache.cpp
+++ ZThreeLevelMemoryPool/page_cache.cpp
@@ -1,1 +1,11 @@
 #include "page_cache.hpp"
+
+void* PageCache::allocateSpan(size_t numPages){
+
+}
+void PageCache::deallocateSpan(void* ptr, size_t numPages){
+
+}
+void PageCache::systemAlloc(size_t numPages){
+    
+}
--- include/page_cache.hpp
+++ include/page_cache.hpp
@@ -12,6 +12,15 @@ class PageCache{
         void deallocateSpan(void* ptr, size_t numPages);
     private:
         PageCache() = default;
-
+        void systemAlloc(size_t numPages);
+    private:
+        struct Span{
+            void* pageAddr; // 页起始地址
+            size_t numPages;
+            Span* next;
+        };  
+        map<size_t, Span*> freeSpans; // size_t代表页数，这里按页数管理不同空闲链表
+        map<void*, Span*> spanMap;   // 维护地址到span的映射，便于合并操作
+        std::mutex mutex;
 };
 #endif
