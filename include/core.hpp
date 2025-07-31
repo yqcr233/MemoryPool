5361bee248fcdbd0852aee29b91bffd4f6a7c915
yqcr233 <3060188944@qq.com>
2025-07-31 23:05:44 +0800

    Fix linux bugs

--- include/core.hpp
+++ include/core.hpp
@@ -5,12 +5,10 @@
 #include<memory>
 #include<mutex>
 #include<assert.h>
-#include<windows.h>
 #include<cstdint>
 #include<atomic>
 #include<array>
 #include<unordered_map>
 #include<map>
-#include<sys/mman.h>
 using namespace std;
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "test.hpp"
 
 int main(){
-    SetConsoleOutputCP(65001); // 设置控制台输出为 UTF-8
     // test_simpel_memory_pool();
     test_three_level_memory_pool();
 }
