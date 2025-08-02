ef58319a2d1a00c5566df180734c6ca27a195645
yqcr233 <3060188944@qq.com>
2025-08-02 22:19:24 +0800

    init for linux

--- include/core.hpp
+++ include/core.hpp
@@ -5,10 +5,12 @@
 #include<memory>
 #include<mutex>
 #include<assert.h>
-#include<windows.h>
 #include<cstdint>
 #include<atomic>
 #include<array>
 #include<unordered_map>
+#include<map>
+#include<sys/mman.h>
+#include<cstring>
 using namespace std;
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "test.hpp"
 
 int main(){
-    SetConsoleOutputCP(65001); // 设置控制台输出为 UTF-8
+    // SetConsoleOutputCP(65001); // 设置控制台输出为 UTF-8
     // test_simpel_memory_pool();
     test_three_level_memory_pool();
 }
